773086a39a6ab12e52703ed1d5a64de8644b3f86
Westknight <samemy1@hotmail.com>
2022-09-26 16:18:41 -0500

    fix 5

--- models/contact.js
+++ models/contact.js
@@ -1,14 +1,14 @@
 const Model = require('./model');
 module.exports =
     class Contact extends Model {
-        constructor(name, phone, email) {
+        constructor(title, phone, email) {
             super();
-            this.Name = name !== undefined ? name : "";
+            this.Title = title !== undefined ? title : "";
             this.Phone = phone !== undefined ? phone : "";
             this.Email = email !== undefined ? email : "";
 
-            this.setKey("Name");
-            this.addValidator('Name', 'string');
+            this.setKey("Title");
+            this.addValidator('Title', 'string');
             this.addValidator('Phone', 'phone');
             this.addValidator('Email', 'email');
         }
